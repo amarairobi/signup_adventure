61f0c8dc833d17c64ac9b156126e9c4c08c88199
Amara Irobi <beaamara6@gmail.com>
2025-10-29 19:12:46 -0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+
+import 'package:flutter/material.dart';
+import 'screens/welcomeScreen.dart';
+
+void main() {
+  runApp(const SignupAdventureApp());
+}
+
+class SignupAdventureApp extends StatelessWidget {
+  const SignupAdventureApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Signup Adventure',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        //fontFamily: 'Helvetica',//
+      ),
+      home: const WelcomeScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
